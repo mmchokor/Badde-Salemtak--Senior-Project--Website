905ceeaf3bead332a206687340e96e53d73b8b5d
mmchokor <mohameddd701@gmail.com>
2023-04-24 23:11:20 +0300

    optimized images

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import SalemtakLogo from '../../assets/images/BadiSLogo.png'
+import SalemtakLogo from '../../assets/images/BadiSLogo.webp'
 import HamburgerMenu from './HamburgerMenu'
 
 const Navbar = () => {
