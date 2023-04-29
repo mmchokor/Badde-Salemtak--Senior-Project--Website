a4959acdb79066bbe8edccee2c58c6a6db6b949a
mmchokor <mohameddd701@gmail.com>
2023-04-29 20:38:43 +0300

    Completed Resident Listing Admin Table

--- src/api/residentListingsAPI.ts
+++ src/api/residentListingsAPI.ts
@@ -6,7 +6,7 @@ import { API_URL } from './apiURL'
 const getResidentListings = async (): Promise<ResidentListing[]> => {
    const response: ResidentResponse = await axios.get(`${API_URL}/resident`, {
       headers: {
-         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWJiMGYzYzFiNWExMzQzNDhjY2RjNCIsImlhdCI6MTY4MTAzNjM5MywiZXhwIjoxNjgzNjI4MzkzfQ.HtQl6PlCF1qrfBbcI482YFxebzCIfVeRgSR0yioD20c`,
+         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NGQ0NTE4MTY3YzE3YWU4NzE3YTM5YSIsImlhdCI6MTY4Mjc4NTU2MCwiZXhwIjoxNjg1Mzc3NTYwfQ.Jeper--GjVuG5oDzBvmFCYIU-WPTrv9O3kjcykZw9yU`,
       },
    })
    return response.data.data.residentListings
@@ -14,21 +14,17 @@ const getResidentListings = async (): Promise<ResidentListing[]> => {
 
 // Delete a resident listing by ID
 const deleteResidentListingById = async (id: string): Promise<string> => {
-   try {
-      const response: ResidentResponse = await axios.delete(
-         `${API_URL}/resident/${id}`,
-         {
-            headers: {
-               Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZWJiMGYzYzFiNWExMzQzNDhjY2RjNCIsImlhdCI6MTY4MTAzNjM5MywiZXhwIjoxNjgzNjI4MzkzfQ.HtQl6PlCF1qrfBbcI482YFxebzCIfVeRgSR0yioD20c`,
-            },
-         }
-      )
-      return response.data.status
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-   } catch (error: any) {
-      throw error.response.data
-   }
+   console.log(`${API_URL}/resident/${id}`)
+   const response: ResidentResponse = await axios.delete(
+      `${API_URL}/resident/${id}`,
+      {
+         headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NGQ0NWVhZTZiY2M0MTY2ODA4YTYzMyIsImlhdCI6MTY4Mjc4NjYwMSwiZXhwIjoxNjg1Mzc4NjAxfQ.XeYnlgj7w6_WD-c38yDoPK7AmyE3OtAsdq24ROldXhw`,
+         },
+      }
+   )
+   console.log(response.data)
+   return response.data.status
 }
 
 export { deleteResidentListingById, getResidentListings }
--- src/pages/admin/Resident.tsx
+++ src/pages/admin/Resident.tsx
@@ -1,12 +1,22 @@
-import { useQuery } from 'react-query'
-import { getResidentListings } from '../../api/residentListingsAPI'
+import { useMutation, useQuery } from 'react-query'
+import {
+    deleteResidentListingById,
+    getResidentListings,
+} from '../../api/residentListingsAPI'
 import { ResidentListing } from '../../global/types'
 
 const Resident = () => {
-   const { isLoading, error, data } = useQuery<ResidentListing[], Error>(
-      'residentListings',
-      getResidentListings
-   )
+   const { isLoading, error, data, refetch } = useQuery<
+      ResidentListing[],
+      Error
+   >('residentListings', getResidentListings)
+
+   const { mutate } = useMutation(deleteResidentListingById, {
+      onSuccess: () => {
+         // Invalidate the query cache to refetch the data
+         refetch()
+      },
+   })
 
    if (isLoading) return <div>Loading...</div>
    if (error) return <div>Error: {error.message}</div>
@@ -17,16 +27,21 @@ const Resident = () => {
          <table className='w-full table-auto'>
             <thead>
                <tr>
+                  <th className='px-4 py-2'>ID</th>
                   <th className='px-4 py-2'>Name</th>
                   <th className='px-4 py-2'>Description</th>
                   <th className='px-4 py-2'>Image</th>
                   <th className='px-4 py-2'>Price</th>
+                  <th className='px-4 py-2'>Created At</th>
+                  <th className='px-4 py-2'>User ID</th>
                   <th className='px-4 py-2'>Quantity</th>
+                  <th className='px-4 py-2'>Actions</th>
                </tr>
             </thead>
             <tbody>
                {data?.map((listing) => (
                   <tr key={listing._id}>
+                     <td className='border px-4 py-2'>{listing._id}</td>
                      <td className='border px-4 py-2'>{listing.name}</td>
                      <td className='border px-4 py-2'>{listing.description}</td>
                      <td className='cursor-pointer border px-4 py-2'>
@@ -35,7 +50,14 @@ const Resident = () => {
                         </a>
                      </td>
                      <td className='border px-4 py-2'>{listing.price}</td>
+                     <td className='border px-4 py-2'>{listing.createdAt}</td>
+                     <td className='border px-4 py-2'>{listing.user._id}</td>
                      <td className='border px-4 py-2'>{listing.quantity}</td>
+                     <td className='border px-4 py-2'>
+                        <button onClick={() => mutate(listing._id)}>
+                           Delete
+                        </button>
+                     </td>
                   </tr>
                ))}
             </tbody>
