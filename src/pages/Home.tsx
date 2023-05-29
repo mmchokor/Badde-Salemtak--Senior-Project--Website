6304a1ea64bc8929577a383aa4dfd5be27ade0be
Rami ElSkakini <sakakinirami2@gmail.com>
2023-05-29 11:22:01 +0300

    fixed image overflow on footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import Landing from '../components/Landing'
-
+import Landing from "../components/Landing";
 const Home = () => {
-   return (
-      <div>
-         <Landing />
-      </div>
-   )
-}
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
